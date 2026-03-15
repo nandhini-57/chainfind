eed191638bd98ea13d9f3ff9eebddea22cf9e753
Nandhini <nandhini0n0@gmail.com>
2026-03-15 23:29:01 +0530

    Delete run.bat

--- run.bat
+++
@@ -1,7 +0,0 @@
-te_path>
-c:/Users/Nandhini/OneDrive/Attachments/my projects/lost&found3/chainfind/run.bat</absolute_path>
-<parameter name="content">
-@echo off
-echo Starting ChainFind Backend...
-cd /d "%~dp0backend"
-python main.py
